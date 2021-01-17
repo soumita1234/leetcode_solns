f0515702e722bdf89f656edb5f1727f4158bc42b
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-01-18 01:46:54 +0530

    Update Roman To Integer.cpp
    my sol

--- Roman To Integer.cpp
+++ Roman To Integer.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> T = { { 'I' , 1 }, { 'V' , 5 }, { 'X' , 10 }, { 'L' , 50 }, { 'C' , 100 }, { 'D' , 500 }, { 'M' , 1000 } };
-        int sum = T[s.back()];
-        for(int i = s.length() - 2; i >= 0; --i){
-            sum +=  (T[s[i]] < T[s[i+1]] ? -T[s[i]] : T[s[i]]);
+        unordered_map<char,int>mpp={
+            {'I', 1},
+            {'V' , 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D',500},
+            {'M',1000}  
+        };
+        int sum=mpp[s.back()];
+        for(int i=s.length()-2;i>=0;--i){
+            sum+= (mpp[s[i]]<mpp[s[i+1]]?-mpp[s[i]]:mpp[s[i]]);
         }
         return sum;
+        
     }
 };
