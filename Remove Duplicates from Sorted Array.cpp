88d7adb0a5068d54abb79685e8ee722bc4567035
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-01-19 02:06:53 +0530

    Update Remove Duplicates from Sorted Array.cpp

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0;
+        if (nums.empty()) return 0; // make this or get WA for empty string
         int r = 0;
         for (int i = 1; i < nums.size(); i ++) {
             if (nums[r] != nums[i]) {
-                nums[++r] = nums[i]; // copy-in next unique number
+                nums[++r] = nums[i]; // do ++r or if r++..It will give 2,1 rather than 1,2 
             }
         }
         return r+1;
