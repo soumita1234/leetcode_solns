fcdbef1255255d69d7fdb71a2b259aeb5f84a3a0
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-02-15 02:14:30 +0530

    Update Plus One.cpp

--- Plus One.cpp
+++ Plus One.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& digits) { //O(1) space   //O(n) time
-        int i = digits.size()-1;
-        
-        if (digits[i] != 9){
-            digits[i]++;
-            return digits;
-        }
-        
-        while (digits[i] == 9){
-            if (i == 0){
-                digits[i] = 1;
-                digits.push_back(0);
+    vector<int> plusOne(vector<int>& digits) { 
+        int ind=digits.size()-1;
+        while(ind>=0){
+            if(digits[ind]==9){
+                digits[ind]=0;
+            }
+            else{
+                digits[ind]+=1;
                 return digits;
             }
-            
-            digits[i] = 0;
-            i--;
+            ind--;
         }
-        digits[i]++;
+        digits.insert(digits.begin(),1);
         return digits;
     }
 };
