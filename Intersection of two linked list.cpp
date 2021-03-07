67945a6af612503b9a836cae59744da14908fdc6
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-03-08 01:17:44 +0530

    Create Intersection of two linked list.cpp

---
+++ Intersection of two linked list.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* tempA=headA;
+        ListNode* tempB=headB;
+        while(tempA!=tempB){
+            tempA= tempA!=NULL ? tempA->next : headB;
+            tempB= tempB!=NULL ? tempB->next : headA;
+        }
+        return tempA;
+    }
+};
