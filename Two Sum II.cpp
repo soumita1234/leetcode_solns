b2a869ab14d8b3b43db0d689120da68aeb4ce44f
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-03-27 23:40:16 +0530

    Create Two Sum II.cpp

---
+++ Two Sum II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l= 0, r = numbers.size()-1, sum;
+        while(l<r){
+            sum = numbers[l] + numbers[r];
+            if (sum == target) break;
+            if(sum < target) ++l;
+            else --r;
+        }
+        return vector<int> {l+1, r+1};
+    }
+};
