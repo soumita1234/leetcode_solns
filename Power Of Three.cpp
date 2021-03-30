ae80e37cedead921a291c1f2a04728f3d4aa6d8e
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-03-31 00:59:20 +0530

    Create Power Of Three.cpp

---
+++ Power Of Three.cpp
@@ -0,0 +1,15 @@
+//1st approach
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<1) return false;
+        while(n%3==0){
+            n=n/3;
+        }
+        return n==1;
+    }
+};
+
+
+
