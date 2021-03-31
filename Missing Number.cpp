c4c7132c394a3d0dfa9e519ad2360bb629509f6d
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-03-31 23:58:18 +0530

    Update Missing Number.cpp

--- Missing Number.cpp
+++ Missing Number.cpp
@@ -17,3 +17,16 @@ public:
         return -1;
     }
 };
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0; 
+        for (int i = 0; i < nums.size(); ++i) {
+            ans += i+1; 
+            ans -= nums[i]; 
+        }
+        return ans; 
+    }
+};
