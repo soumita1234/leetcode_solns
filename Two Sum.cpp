8ca426cae312a19e81ee1cd00f6811eecc020823
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-04-10 20:45:37 +0530

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -1,3 +1,25 @@
+//naive approach
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> res;
+        
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    res = {i,j};
+                    return res;
+                }
+            }
+        }
+        
+        return res;   
+    }
+};
+
+
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
