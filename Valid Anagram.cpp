0169ce0e3ffa0f09b9da19692d14cbd3abf8c2cd
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-04-19 23:09:34 +0530

    Update Valid Anagram.cpp

--- Valid Anagram.cpp
+++ Valid Anagram.cpp
@@ -7,3 +7,23 @@ public:
         return s==t;
     }
 };
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.size(), m = t.size();
+        if(n != m)
+            return false;
+        
+        int count1[26] = {0}, count2[26] = {0};
+        for(int i = 0; i < n; i++)
+        {
+            count1[s[i] - 'a']++;
+            count2[t[i] - 'a']++;
+        }
+        for(int i = 0; i < 26; i++)
+            if(count1[i] != count2[i])
+                return false;
+        return true;
+    }
+};
