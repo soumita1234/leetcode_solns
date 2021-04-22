b27fcd6f1b9999dabb0fc3ce7e07471979414624
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-04-22 22:21:03 +0530

    Create Move Zeroes.cpp

---
+++ Move Zeroes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==0){
+                    swap(nums[i],nums[j]);
+                }
+            }
+        }
+        
+        
+    }
+};
+
+
+
+
+void moveZeroes(vector<int>& nums) {
+    for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.size(); cur++) {
+        if (nums[cur] != 0) {
+            swap(nums[lastNonZeroFoundAt++], nums[cur]);
+        }
+    }
+}
