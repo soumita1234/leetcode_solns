a66a3761930e099a1a32666e2cd25f8dd9cec4ef
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-05-03 23:52:08 +0530

    Update Hamming Distance.cpp

--- Hamming Distance.cpp
+++ Hamming Distance.cpp
@@ -13,3 +13,22 @@ public:
         return cnt;
     }
 };
+
+
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) 
+    {
+        int res = x^y;
+        int cnt=0;
+        
+        while(res > 0)
+        {
+            
+            cnt+=res & 1;
+            res >>= 1;
+        }
+        return cnt;
+    }
+};
