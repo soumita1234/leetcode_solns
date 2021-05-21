252db38d1059f4283f4f80b47b1e8db851cec221
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-05-21 15:23:09 +0530

    Create Sort List.cpp

---
+++ Sort List.cpp
@@ -0,0 +1,82 @@
+//Merge Sort Method(AC)
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+
+    ListNode getMid(ListNode head) {
+        ListNode midPrev = null;
+        while (head != null && head.next != null) {
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+        ListNode mid = midPrev.next;
+        midPrev.next = null;
+        return mid;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// Selection sort method(TLE)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        ListNode* cur= head;
+        while(cur!=NULL){
+            ListNode* it=cur;
+            ListNode* min=it;
+            while(it!=NULL){
+                if(it->val<min->val)
+                    min=it;
+                it=it->next;
+            }
+            int temp=cur->val;
+            cur->val=min->val;
+            min->val=temp;
+            cur=cur->next;
+        }
+        return head;
+    }
+};
