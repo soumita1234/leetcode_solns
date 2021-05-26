34749da0a4236add364d6e952167f66eb713fd4d
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-05-26 13:34:38 +0530

    Update Third Maximum Number.cpp

--- Third Maximum Number.cpp
+++ Third Maximum Number.cpp
@@ -18,3 +18,17 @@ public:
 };
 
 
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end(),greater<int>());
+        nums.erase(unique(nums.begin(),nums.end()),nums.end());
+        if(nums.size() < 3)
+            return nums[0];
+        
+        return nums[2];
+        
+    }
+};
+
