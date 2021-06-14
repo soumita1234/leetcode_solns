3c4c6e9e0a7e72022b4c3d63bf454a4ffb2c4d4d
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-06-14 17:49:15 +0530

    Create Duplicate Zeros.cpp

---
+++ Duplicate Zeros.cpp
@@ -0,0 +1,17 @@
+void duplicateZeros(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        if (arr[i] == 0)
+        {
+            for (int j = arr.size() - 1; j > i + 1; j--)
+            {
+                arr[j] = arr[j - 1];
+            }
+            arr[i + 1] = 0;
+            i++;
+        }
+    }
+    return;
+}
+};
