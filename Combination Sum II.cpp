f12aef7455614d44c7823dfed13f9ba8ae057f3b
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-06-21 18:40:50 +0530

    Create Combination Sum II.cpp

---
+++ Combination Sum II.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(vector<int>& cand, int sum, int start, vector<int> v){
+        if(sum < 0){    // if sum < 0 solution is not found.
+            return;
+        }
+        else if(sum == 0){  // if sum == 0 it means we have found a solution.
+            ans.push_back(v);
+            return;
+        }
+        for(int i=start;i<cand.size();i++){
+            if(i > start && cand[i] == cand[i-1]){  // To avoid duplicates.
+                continue;
+            }
+            if(sum-cand[i] < 0) {   // No point of searching further if sum-cand[i] < 0.
+                break;
+            }
+            // Backtracking
+            v.push_back(cand[i]);
+            solve(cand,sum-cand[i],i+1,v);
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& cand, int target) {
+        sort(cand.begin(), cand.end());
+        vector<int> v;
+        solve(cand, target, 0, v);
+        return ans;
+    }
+};
