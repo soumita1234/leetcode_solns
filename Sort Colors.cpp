2e0485560ef8dfe5e189552a355d54e7a0a4ba63
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-07-07 20:20:46 +0530

    Update Sort Colors.cpp

--- Sort Colors.cpp
+++ Sort Colors.cpp
@@ -19,3 +19,35 @@ public:
         }
     }
 };
+
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int one=0;
+        int two=0;
+        int zero=0;
+        for(auto it: nums){
+            if(it==0) zero++;
+            if(it==1) one++;
+            if(it==2) two++;
+        }
+        int i=0;
+        while(zero>0){
+            nums[i]=0;
+            i++;
+            zero--;
+        }
+        while(one>0){
+            nums[i]=1;
+            i++;
+            one--;
+        }
+        while(two>0){
+            nums[i]=2;
+            i++;
+            two--;
+        }
+    }
+};
