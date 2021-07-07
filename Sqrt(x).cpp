8b4e08a4b110baaa30eb394e48834552f75872ba
Soumita Biswas <58908901+soumita1234@users.noreply.github.com>
2021-07-07 12:16:11 +0530

    Update Sqrt(x).cpp

--- Sqrt(x).cpp
+++ Sqrt(x).cpp
@@ -4,3 +4,27 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        long start=0;
+        long end=x;
+        
+        while(start+1<end){
+            long mid= start+(end-start)/2;
+            if(mid*mid==x){
+                return (int)mid;
+            }
+            if(mid*mid<x){
+                start=mid;
+            }
+            else{
+                end=mid;
+            }
+        }
+        if(end*end==x) return (int)end;
+        return (int)start;
+    }
+};
